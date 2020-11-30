6e91b829bf47ad6eeaacd9720054e033b277ca68
Youssef Hamdi <youssefhamdih11@gmail.com>
2020-11-30 21:49:26 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import Main from './components/MainComponent';
 import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   
   render (){
     return (
-      <div>
-        <Main/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
